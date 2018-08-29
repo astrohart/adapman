bb7732be5cab4d5305f6aacfca7946788d3a86ba
Brian Hart <brian.hart@xylogix.net>
2018-08-29 12:10:56 -0400

    29 Aug 2018 Cleaned up the code, and added documentation comments, to the NetworkAdapter class

--- adapman/NetworkAdapter.cs
+++ adapman/NetworkAdapter.cs
@@ -8,16 +8,26 @@ namespace adapman
     public class NetworkAdapter
     {
         /// <summary>
-        /// Constructs a new instance of <see cref="T:adapman.NetworkAdapter"/>
+        /// Constructs a new instance of <see cref="T:adapman.NetworkAdapter" />
         /// and returns a refernce to it.
         /// </summary>
-        /// <param name="deviceId">Integer specifying the Windows device ID of the adapter.</param>
-        /// <param name="productName">String specifying the product name of the adapter.</param>
-        /// <param name="description">String containing the description listed
-        /// in the system for the adapter.</param>
-        /// <param name="enabled">Flag indicating whether this adapter is enabled.</param>
-        /// <param name="connStatus">Integer value indicating the connected state of the adapter.</param>
-        public NetworkAdapter(int deviceId, string productName, 
+        /// <param name="deviceId">
+        /// Integer specifying the Windows device ID of the adapter.
+        /// </param>
+        /// <param name="productName">
+        /// String specifying the product name of the adapter.
+        /// </param>
+        /// <param name="description">
+        /// String containing the description listed in the system for the
+        /// adapter.
+        /// </param>
+        /// <param name="enabled">
+        /// Flag indicating whether this adapter is enabled.
+        /// </param>
+        /// <param name="connStatus">
+        /// Integer value indicating the connected state of the adapter.
+        /// </param>
+        public NetworkAdapter(int deviceId, string productName,
             string description, bool enabled, int connStatus)
         {
             DeviceID = deviceId;
@@ -28,17 +38,20 @@ namespace adapman
         }
 
         /// <summary>
-        /// Gets an integer value that specifies the connected state of this adapter.
+        /// Gets an integer value that specifies the connected state of this
+        /// adapter.
         /// </summary>
         public int ConnStatus { get; }
 
         /// <summary>
-        /// Gets a string containing the description of this network adapter as listed in the system.
+        /// Gets a string containing the description of this network adapter as
+        /// listed in the system.
         /// </summary>
         public string Description { get; }
 
         /// <summary>
-        /// Gets an integer value specifying the Windows device ID of this adapter.
+        /// Gets an integer value specifying the Windows device ID of this
+        /// adapter.
         /// </summary>
         public int DeviceID { get; }
 
@@ -48,12 +61,14 @@ namespace adapman
         public bool IsEnabled { get; private set; }
 
         /// <summary>
-        /// Gets a string containing the product name of this adapter as listed by the operating system.
+        /// Gets a string containing the product name of this adapter as listed
+        /// by the operating system.
         /// </summary>
         public string ProductName { get; }
 
         /// <summary>
-        /// Disables this network adapter (i.e., makes it unusable for connecting to networks).
+        /// Disables this network adapter (i.e., makes it unusable for connecting
+        /// to networks).
         /// </summary>
         public void Disable()
         {
@@ -61,7 +76,8 @@ namespace adapman
         }
 
         /// <summary>
-        /// Enables this network adapter (sets it as being able to connect to networks).
+        /// Enables this network adapter (sets it as being able to connect to
+        /// networks).
         /// </summary>
         public void Enable()
         {
@@ -77,7 +93,8 @@ namespace adapman
             {
                 var currentManagementObject = new ManagementObject();
                 var queryString =
-                    $"SELECT DeviceID,ProductName,Description,NetEnabled FROM Win32_NetworkAdapter WHERE DeviceID = {DeviceID}";
+                    "SELECT DeviceID,ProductName,Description,NetEnabled FROM " +
+                    $"Win32_NetworkAdapter WHERE DeviceID = {DeviceID}";
 
                 var query = new ObjectQuery(queryString);
                 var searcher = new ManagementObjectSearcher(query);
@@ -85,11 +102,11 @@ namespace adapman
 
                 foreach (var managementBaseObject in results)
                 {
-                    currentManagementObject = (ManagementObject) managementBaseObject;
+                    currentManagementObject = (ManagementObject)managementBaseObject;
                 }
 
-                //Enable the Network Adapter by invoking the appropriate method on the 
-                // management object found
+                // Enable the Network Adapter by invoking the appropriate method
+                // on the WMI management object found by the query above
                 currentManagementObject.InvokeMethod("Enable", null);
 
                 IsEnabled = true;
@@ -101,25 +118,30 @@ namespace adapman
             }
         }
 
+        /// <summary>
+        /// Implements the code to disable a network adapter.
+        /// </summary>
         private void DoDisable()
         {
             try
             {
-                var currentMObject = new ManagementObject();
-                var strWQuery =
-                    $"SELECT DeviceID,ProductName,Description,NetEnabled FROM Win32_NetworkAdapter WHERE DeviceID = {DeviceID}";
+                var currentManagementObject = new ManagementObject();
+                var queryString =
+                    "SELECT DeviceID,ProductName,Description,NetEnabled FROM " +
+                    $"Win32_NetworkAdapter WHERE DeviceID = {DeviceID}";
 
-                var oQuery = new ObjectQuery(strWQuery);
-                var oSearcher = new ManagementObjectSearcher(oQuery);
-                var oReturnCollection = oSearcher.Get();
+                var query = new ObjectQuery(queryString);
+                var searcher = new ManagementObjectSearcher(query);
+                var results = searcher.Get();
 
-                foreach (var mo in oReturnCollection)
+                foreach (var managementBaseObject in results)
                 {
-                    currentMObject = (ManagementObject) mo;
+                    currentManagementObject = (ManagementObject)managementBaseObject;
                 }
 
-                //Disable the Network Adapter
-                currentMObject.InvokeMethod("Disable", null);
+                // Disable the Network Adapter by invoking the appropriate method
+                // on the management object found
+                currentManagementObject.InvokeMethod("Disable", null);
 
                 IsEnabled = false;
             }
